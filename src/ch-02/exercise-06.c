d6f14aa333f2ace5f0a81706a1b8b2912e5ef194
arturop <67555134+arturopstem@users.noreply.github.com>
2024-01-16 18:13:38 -0500

    Chapter 02 - Exercise 06

---
+++ src/ch-02/exercise-06.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int toes;
+    toes = 10;
+
+    printf("toes: %d, toes_twice: %d, toes_squared: %d\n", toes, toes * 2, toes * toes);
+
+    return 0;
+}
