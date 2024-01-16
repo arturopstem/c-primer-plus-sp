497c304a0db9fd9d9c1f017d271d103e144f96cd
arturop <67555134+arturopstem@users.noreply.github.com>
2024-01-16 18:25:27 -0500

    Chapter 02 - Exercise 08

---
+++ src/ch-02/exercise-08.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void one_tree(void);
+void two(void);
+
+int main(void)
+{
+    printf("starting now:\n");
+    one_tree();
+    printf("done!\n");
+    return 0;
+}
+
+void one_tree(void)
+{
+    printf("one\n");
+    two();
+    printf("three\n");
+}
+
+void two(void)
+{
+    printf("two\n");
+}
