052855e269774ea978a60ea7a681036435f7ee97
arturop <67555134+arturopstem@users.noreply.github.com>
2024-01-19 04:10:16 -0500

    Chapter 05 - Exercise 06

---
+++ src/ch-05/exercise-06.c
@@ -0,0 +1,18 @@
+/* addemup.c -- five kinds of statements */
+#include <stdio.h>
+
+int main(void) /* finds sum of first cound integers */
+{
+    int count, sum; /* declaration statement */
+    int num;
+
+    count = 0; /* assignment statement */
+    sum = 0;   /* ditto */
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    while (count++ < num)          /* while */
+        sum = sum + count * count; /* statement */
+    printf("sum = %d\n", sum);     /* function statement */
+
+    return 0; /* return statemenet */
+}
