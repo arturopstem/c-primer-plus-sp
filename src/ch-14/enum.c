95f6f92e31b995809c5d27c50bac514f2ea2c487
arturop <67555134+arturopstem@users.noreply.github.com>
2024-02-05 23:55:49 -0500

    Listing 14.15 - The enum.c Program

---
+++ src/ch-14/enum.c
@@ -0,0 +1,107 @@
+/* enum.c -- uses enumerated values */
+#include <stdbool.h> // C99 feature
+#include <stdio.h>
+#include <string.h> // for strcmp(), strchr()
+
+#define LEN 30
+char *s_gets(char *st, int n);
+
+enum spectrum
+{
+    red,
+    orange,
+    yellow,
+    green,
+    blue,
+    violet,
+};
+
+const char *colors[] = {
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+};
+
+int main(void)
+{
+    char choice[LEN];
+    enum spectrum color;
+    bool color_is_found = false;
+
+    puts("Enter a color (empty line to quit):");
+    while (s_gets(choice, LEN) != NULL && choice[0] != '\0')
+    {
+        for (color = red; color <= violet; color++)
+        {
+            if (strcmp(choice, colors[color]) == 0)
+            {
+                color_is_found = true;
+                break;
+            }
+        }
+        if (color_is_found)
+        {
+            switch (color)
+            {
+            case red:
+                puts("Roses are red.");
+                break;
+            case orange:
+                puts("Poppies are orange.");
+                break;
+            case yellow:
+                puts("Sunflowers are yellow.");
+                break;
+            case green:
+                puts("Grass is green.");
+                break;
+            case blue:
+                puts("Bluebells are blue.");
+                break;
+            case violet:
+                puts("Violets are violet.");
+                break;
+
+            default:
+                break;
+            }
+        }
+        else
+        {
+            printf("i don't know about the color %s.\n", choice);
+        }
+        color_is_found = false;
+        puts("Next color, please (empty line to quit):");
+    }
+    puts("Goodbye!");
+
+    return 0;
+}
+
+char *s_gets(char *st, int n)
+{
+    char *ret_val;
+    char *find;
+
+    ret_val = fgets(st, n, stdin);
+    if (ret_val)
+    {
+        find = strchr(st, '\n'); // look for newline
+        if (find)                // if the address is not NULL,
+        {
+            *find = '\0'; // place a null character there
+        }
+        else
+        {
+            while (getchar() != '\n')
+            {
+                continue; // dispose of rest of line
+            }
+        }
+    }
+
+    return ret_val;
+}
